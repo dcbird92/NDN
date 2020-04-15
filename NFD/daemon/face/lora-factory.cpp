6e1794c0e6d34f0ddea78d381e3955e249a1eac7
Jake Maschoff <maschoffjake@gmail.com>
2020-04-15 17:37:41 -0600

    noa rrow.

--- NFD/daemon/face/lora-factory.cpp
+++ NFD/daemon/face/lora-factory.cpp
@@ -40,7 +40,7 @@ LoRaFactory::doCreateFace(const CreateFaceRequest& req,
 {
 
   NFD_LOG_DEBUG("local " << req.localUri->getHost());
-  NFD_LOG_DEBUG("remote " << req.remoteUri->getHost());
+  NFD_LOG_DEBUG("remote " << req.remoteUri.getHost());
   for (const auto& i : m_channels) {
       i.second->createFace(req.params, onCreated);
       return;
