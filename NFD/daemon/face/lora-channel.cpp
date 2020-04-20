68e657a74a066e042f8de1ecb33c52a0890f32a9
Jake Maschoff <maschoffjake@gmail.com>
2020-04-19 18:42:06 -0600

    no matching list, unable to make pair...

--- NFD/daemon/face/lora-channel.cpp
+++ NFD/daemon/face/lora-channel.cpp
@@ -41,9 +41,9 @@ LoRaChannel::LoRaChannel(std::string URI){
 
 
 void
-LoRaChannel::createFace(std::queue<std::pair<std::pair<uint8_t, uint8_t>*, ndn::encoding::EncodingBuffer *>*>* sendBufferQueue, 
-                        pthread_mutex_t& queueMutex,
-                        std::pair<uint8_t, uint8_t>* ids
+LoRaChannel::createFace(std::queue<std::pair<std::pair<uint8_t, uint8_t>*, ndn::encoding::EncodingBuffer *>*>* sendBufferQueue,
+                        pthread_mutex_t* queueMutex,
+                        std::pair<uint8_t, uint8_t>* ids,
                         const FaceParams& params,
                         const FaceCreatedCallback& onFaceCreated,
                         const FaceCreationFailedCallback& onFailure)
